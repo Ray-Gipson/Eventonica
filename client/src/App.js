5d32e22a7a74b5eb4a0d4a99a494a050717de595
ray.gipsonjr@gmail.com <108632022+Ray-Gipson@users.noreply.github.com>
2023-05-06 22:21:35 -0400

    new changes

---
+++ client/src/App.js
@@ -0,0 +1,98 @@
+import calendar from './images/calendar.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={calendar} alt="Calendar Star Logo" />
+        <h1>Eventonica</h1>
+      </header>
+
+      <main>
+        {/* User-management */}
+        <section className="user-management">
+          <h2>User Management</h2>
+
+          <table id="users">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email Address</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>....</td>
+                <td>....</td>
+                <td>....</td>
+                <td>
+                  <button>Edit</button>
+                </td>
+                <td>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div>
+            <h3>Add User</h3>
+            <form id="add-user" action="#">
+              <fieldset>
+                <label>Name</label>
+                <input type="text" id="add-user-name"/>
+              </fieldset>
+              {/* Add more form fields here */}
+              <input type="submit" value="Add"/>
+            </form>  
+          </div>
+        </section>
+        {/* Event-management */}
+        <section className="event-management">
+          <h2>Event Management</h2>
+          <div>
+            <h3>All Events</h3>
+            <ul id="events-list">
+              {/* Display all Events here */}
+              <li>...</li>
+            </ul>
+
+            <h3>Add Event</h3>
+            <form id="add-event" action="#">
+              <fieldset>
+                <label>Name</label>
+                <input
+                  type="text"
+                  id="add-event-name"
+                  placeholder="tech meetup"
+                />
+              </fieldset>
+              {/* Add more form fields here */}
+              <input type="submit" />
+            </form>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <div>
+          Star Calendar favicon made by &nbsp;
+          
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+            Freepik. 
+          </a>    
+          &nbsp;Find your own on &nbsp;
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            FlatIcon.com
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
